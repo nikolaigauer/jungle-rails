e21361bc2a6c6c22da8700e178a375702bd1bc04
Nikolai <nikolaigauer@gmail.com>
2017-08-06 23:50:22 +0000

    Add new test code for reviews controller, add validation in review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,28 +4,37 @@ class ReviewsController < ApplicationController
   # GET /reviews
   # GET /reviews.json
   def create
-    @product = Product.find(params[:product_id])
-
-    @review = @product.reviews.new(review_params)
-
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.product_id = params[:product_id]
     if @review.save
-      redirect_to @product, notice: 'Review was successfully created.'
-    else
-      redirect_to @product, notice: 'Review failed to save.'
+      redirect_to product_path(@review.product_id)
     end
+  end
+
+  def review_params
+    params.require(:review).parmit(:rating, :description)
   end
 
   def destroy
     @review.destroy
-    redirect_to @review.product, notice: 'Review was successfully destroyed.'
+    redirect_to @review.product, notice: 'Review was successfully deleted.'
   end
 
   private
-    def set_review
-      @egg = Review.find(params[:id])
+    def logged_in?
+      if(current_user)
+        true
+      else
+        false
+      end
     end
 
-    def review_params
-      params.require(:review).permit(:description, :rating)
+    def require_login
+
+      unless logged_in?
+        flash[:error] = "you mus be logged in to access this section"
+        redirect_to '/login'
+      end
     end
 end
